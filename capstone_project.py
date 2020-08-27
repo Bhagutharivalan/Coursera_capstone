4260dff11bdbb139dc91064fca68426f7f5bc85d
Bhagutharivalan <66340441+Bhagutharivalan@users.noreply.github.com>
2020-08-26 21:14:26 -0400

    Add files via upload

---
+++ capstone_project.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This project contains the capstone project of course era
+# 
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
